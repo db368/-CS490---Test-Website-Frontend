9600512116a4fac5bcf7cecf1e8e5d55bc8dd54b
Drew Balletto <db368@njit.edu>
2018-06-04 15:35:37 -0400

    Format debugger page, remove unused comments. Added an info.php to assist in solving problems with test server.

--- debug.php
+++ debug.php
@@ -1,5 +1,13 @@
-<?php //debug.php
+<html>
+<head>
+    <title> Debug </title>
+</head>
 
+<body>
+<h1> Debug.php </h1>
+<?php //debug.php
+$target = "https://web.njit.edu/~jll25/CS490/switch.php";
+echo "Posting to: " . $target . "<br>";
 echo "<h3> Post Variables </h3>";
 
 if (!(isset($_POST))) {
@@ -15,13 +23,6 @@ foreach ($_POST as $name => $val){
             echo " " . $name . ':' . $val . "<br>";
     }
 }
-
-$target = "https://web.njit.edu/~jll25/CS490/switch.php";
-//$target = "https://web.njit.edu/~db368/CS490/debug.php"; //Incase you wanted to debug the debugger
-//if (isset($_POST["url"])){
-//        $target = $_POST["url"];
-//}
-
 
 if ($target != "none") {
         $ch = curl_init();
@@ -44,5 +45,6 @@ if ($target != "none") {
     }
 }
 
-
 ?>
+</body>
+</html>
