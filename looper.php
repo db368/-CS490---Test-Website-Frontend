aceb32327817fd484f7b54216049be1e8d57fcec
Drew Balletto <db368@njit.edu>
2018-06-05 15:22:54 -0400

    Oversight with post variable naming

--- looper.php
+++ looper.php
@@ -12,6 +12,8 @@ $return_val=curl_exec($ch);
 curl_close($ch);
 
 //Finally, we load the url that we wanted to redirect to.
+//One little tweak to the post
+$_POST['id'] = $_POST['eid'];
 $target = 'http://afsaccess3.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/eexam.php';
 $ch= curl_init();
 curl_setopt($ch, CURLOPT_URL, "$target");
