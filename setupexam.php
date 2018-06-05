b798440880528cb4783d18017a6c9fafc9206995
db368 <db368@njit.edu>
2018-06-05 13:22:41 -0400

    Make sure this problem isn't on my end

--- setupexam.php
+++ setupexam.php
@@ -1,17 +1,21 @@
 <?php
 //Get the questions for the exam
 $target = "https://web.njit.edu/~jll25/CS490/switch.php";
+//$target = "http://afsaccess3.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/debug.php";
+echo 'attempting to post'.$_POST;
 $ch= curl_init();
-curl_setopt($ch, CURLOPT_URL, "$target");
+curl_setopt($ch, CURLOPT_URL, $target);
 curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'e_get_questions', 'id' => $_POST["id"])));
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('identifier'=>'e_get_questions', 'id' => $_POST["id"]));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $return_val=curl_exec($ch);
 curl_close($ch);
 
-if ($return_val ==null) {
+echo $return_val;
+if ($return_val == null) {
     echo "<h1> ERROR: NO RETURN VALUE </h2>";
 }
+/*
 //Test JSON
 $target = "debug.php";
 $ch2= curl_init();
