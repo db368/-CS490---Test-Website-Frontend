33dd4ee4b42163e0260321949b67500d32bc2d2f
Drew Balletto <db368@njit.edu>
2018-06-05 17:30:58 -0400

    Formatting

--- Druswitch.php
+++ Druswitch.php
@@ -1,7 +1,8 @@
 <?php //Druswitch.php
 
 //First of all, I'm not uploading the password/username to github. That's a bad idea. Instead lets load it from a secret config file.
-if (!isset($_POST['identifier'])) {echo "No identifier!";}
+if (!isset($_POST['identifier'])) {echo "No identifier!";
+}
 //echo var_dump($_POST);
 //echo "Is this thing on?";
 $secretfilepath = "secret.ini";
@@ -11,8 +12,7 @@ $password = $up['password'];
 // Testing this is fun
 
 $datab = new mysqli("sql1.njit.edu", $username, $password, $username);
-if (mysqli_connect_errno()) 
-{echo "Something went wrong";
+if (mysqli_connect_errno()) {echo "Something went wrong";
 }
 
 //Lets hope we make it this far
@@ -21,13 +21,12 @@ case "aq_exam":
     $eid = $_POST['eid'];
     $qid = $_POST['qid'];
     $score = $_POST['score'];
- 
-    $add ="INSERT INTO ExQuestions (Exam_id, Question_id, Total_points) 
+
+    $add ="INSERT INTO ExQuestions (Exam_id, Question_id, Total_points)
 	   VALUES ('$eid', '$qid', '$score')";
 
-sleep(1); //Give the DB some time to update
-if (!$datab->query($add))
-    {
+    sleep(1); //Give the DB some time to update
+    if (!$datab->query($add)) {
         echo "FAILURE!";
     }
 
