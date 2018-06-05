637b62336cccd0fb88c7b5280ad4d3f0ca610eba
Drew Balletto <db368@njit.edu>
2018-06-05 11:14:17 -0400

    Begin bringing addq online
    Waiting on backend.

--- addq.php
+++ addq.php
@@ -23,19 +23,34 @@
 }
 </style>
 <head>
-    <title> Add a Question </title>
+    <title> Modify/Add Question </title>
 </head>
 
 <body>
-    <h1> Add A question! </h1>
+    <h1> Modify Question </h1>
 <?php
-    //Will need a lot of issets.
+    //Obtain Question from Database
+    $target = "https://web.njit.edu/~jll25/CS490/switch.php";
+    $ch= curl_init();
+    curl_setopt($ch, CURLOPT_URL, "$target");
+    curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'qb_get_question','questionid'=>$_POST['qid'])));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $return_val=curl_exec($ch);
+    curl_close($ch);
+    if ($return_val == null) {
+        echo "<h1> ERROR: NO RETURN VALUE </h1>";
+        exit;
+    }
+    //Begin Table
+    /* Debug Code.
     $qtext = "Question Text";
     $testcases= array("Testcase 1", "Testcase 2", "Testcase 3");
     $solutions = array("Solution 1", "Solution 2", "Solution 3");
     $almagamation = array_combine($testcases, $solutions);
     $diff = "Easy";
-    $purpose = "a_testbank";
+    $purpose = "a_testbank";*/
+
     echo '<form action="debug.php" method="post">';
 foreach (array("Easy", "Medium", "Hard") as $rdiff){
     echo '<input type="radio" name=difficulty value="' . $rdiff . '"';
