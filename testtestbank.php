4eccd9b8e27aa4e8fb42446c102eb767a737fe48
Drew Balletto <db368@njit.edu>
2018-06-05 09:51:13 -0400

    Expand testtestbank to also test the v_exams identifier

--- testtestbank.php
+++ testtestbank.php
@@ -2,6 +2,11 @@
 <body>
     <form action="debug.php" method="post">
     <input type="hidden" name="identifier" value="v_testbank">
-    <button type="submit" class="link-button"> Submit </button>
+    <button type="submit" class="link-button"> send v_textbank </button>
+</form>
+
+    <form action="debug.php" method="post">
+    <input type="hidden" name="identifier" value="v_exams">
+    <button type="submit" class="link-button"> send v_exams </button>
 </body>
 </html>
