41fd09752104f9862b117f5f922e1d2e035123c4
Drew Balletto <db368@njit.edu>
2018-06-05 09:31:56 -0400

    Curls probably don't work from my desktop.

--- debug.php
+++ debug.php
@@ -7,6 +7,9 @@
 <h1> Debug.php </h1>
 <?php //debug.php
 $target = "https://web.njit.edu/~jll25/CS490/switch.php";
+//$target = "https://http://afsaccess3.njit.edu/~jll25/CS490/switch.php";
+//$target = "http://afsaccess3.njit.edu/~db368/CS490/debug.php";
+
 echo "Posting to: " . $target . "<br>";
 echo "<h3> Post Variables </h3>";
 
@@ -26,17 +29,17 @@ foreach ($_POST as $name => $val){
 
 if ($target != "none") {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $target);
+        curl_setopt($ch, CURLOPT_URL, "$target");
         curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
         curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $return_val=curl_exec($ch);
         curl_close($ch);
-        if ($return_val == null)
-        {
-            echo "<h1 color=red> RETURNED NULL <h1>";
-            exit;
-        }
+    if ($return_val == null) {
+        echo "<h1> RETURNED NULL <h1>";
+        exit;
+    }
         echo "<h3> Raw Returned Json: </h3>" .  $return_val . "<br> <br>";
 
         echo "<h3> var_dump of json_decode for returned json:</h3> ";
