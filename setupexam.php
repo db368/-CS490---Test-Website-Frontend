b4c296713e09f7f3cd380e82e573e0f556dbace4
db368 <db368@njit.edu>
2018-06-06 09:04:10 -0400

    Guess Qids, since that data isn't actually supposed to be sent by the backend

--- setupexam.php
+++ setupexam.php
@@ -10,45 +10,70 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $return_val=curl_exec($ch);
 curl_close($ch);
 
-echo $return_val;
+//echo $return_val;
 if ($return_val == null) {
     echo "<h1> ERROR: NO RETURN VALUE </h2>";
 }
-/*
-Test JSON
-$target = "debug.php";
-$ch2= curl_init();
-curl_setopt($ch2, CURLOPT_URL, "$target");
-curl_setopt($ch2, CURLOPT_POST, 1); // Set it to post
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, http_build_query($return_val));
-$return_debug=curl_exec($ch2);
-curl_close($ch2);
-*/
-//echo $return_debug;
 
-$qjson = json_decode($return_val, true);
-$i=0;
-$qarray = array();
-foreach($qjson as $question){
-    $qarray[$i]=$question['id']; // Build a new array with numbers as keys
-    $i++;
+//Get Qbank 
+$ch3= curl_init();
+curl_setopt($ch3, CURLOPT_URL, "$target");
+curl_setopt($ch3, CURLOPT_POST, 1); // Set it to post
+curl_setopt($ch3, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch3, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'v_testbank')));
+$qb_return=curl_exec($ch3);
+curl_close($ch3);
+
+
+$uqarray = array(); //Final destination for question ids
+$eqarray = json_decode($return_val, true); //Question Bank Array
+$qbarray = json_decode($qb_return, true); //Exam Questions
+
+echo "<h1> DUMPING QUESTION BANK </h1> <br>";
+echo var_dump($qbarray). '<br>';
+echo "<h1> DUMPING EXAM QUESTIONS </h1> <br>";
+echo var_dump($eqarray). '<br>';
+
+//God Tier Hacks Incoming
+//So e_get_questions doesn't return id. That's really bad.
+//We can get around that though by comparing the question text and difficulty with questions
+//in the question bank
+$usedids = array(); //Make a little sub array to handle duplicates
+//echo "Sorting...<br>";
+//echo "<h1> DUMPING ULTIMATE ARRAY </h1> <br>";
+foreach ($eqarray as $eq){
+	$eqtext = $eq['Question'];
+	$eqdiff = $eq['Difficulty'];
+	foreach($qbarray as $qb){
+		$qbtext = $qb['Question'];
+		$qbid = $qb['Qid'];
+		if ($qbtext == $eqtext and !in_array($qbid, $usedids)){ //Compare Question Text
+			array_push($usedids, $qb['Qid']);
+			$eq['id'] = $qb['Qid'];
+			array_push($uqarray, $eq);
+			break;
+		}
+	}
 }
 
-$alarray = array(); //Build an almighty array to hold the questions and manage other info
-$alarray['questions']=$qarray;
-$alarray['currentquestion'] = '1';
+echo var_dump($uqarray);
 
-//$target = "debug.php";
-$target = "http://afsaccess3.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/debug.php";
+
+
+
+
+
+/*
+$target = "http://afsaccess3.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/texam.php";
+//$target = "http://afsaccess3.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/debug.php";
 $ch2= curl_init();
 curl_setopt($ch2, CURLOPT_URL, "$target");
 curl_setopt($ch2, CURLOPT_POST, 1); // Set it to post
-curl_setopt($ch2, CURLOPT_POSTFIELDS, http_build_query($alarray));
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $alarray);
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 $page = curl_exec($ch2);
 curl_close($ch2);
 
 echo $page;
-
+*/
 ?>
