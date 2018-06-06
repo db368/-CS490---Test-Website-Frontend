7a4afd706b55108a11d5cd8b0690ea1823398946
Drew Balletto <db368@njit.edu>
2018-06-06 10:45:33 -0400

    Comment out debug code, format

--- texam.php
+++ texam.php
@@ -8,21 +8,21 @@
     $number = $_POST['currentquestion'];
     $exid = $_POST['exid'];
     //On first run answers will not be set
-    if (isset($_POST['answers'])){
-	 $answers = $_POST['answers'];
-    }
-    else {
-	$answers = array(); 
-    }
+if (isset($_POST['answers'])) {
+    $answers = $_POST['answers'];
+}
+else {
+    $answers = array();
+}
     $qid = $questions[$number];
-    echo "<h1> Incoming POST </h1>"; 
-    echo var_dump($_POST);
+    //echo "<h1> Incoming POST </h1>";
+    //echo var_dump($_POST);
 
-    echo "<h1> Incoming questions </h1>"; 
-    echo var_dump($questions);
-    echo "<br> Current Question Number is ". $number. " and the id is ". $qid . "<br>";
+    //echo "<h1> Incoming questions </h1>";
+    //echo var_dump($questions);
+    //echo "<br> Current Question Number is ". $number. " and the id is ". $qid . "<br>";
 
-   //CURL to get the actual useful question information; 
+   //CURL to get the actual useful question information;
     $target = "https://web.njit.edu/~jll25/CS490/switch.php";
     $ch= curl_init();
     curl_setopt($ch, CURLOPT_URL, $target);
@@ -31,14 +31,14 @@
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $return_val=curl_exec($ch);
     curl_close($ch);
-    
-    echo "<h1> RETURN FROM THE SERVER </h1> <br>";	
+
+    //echo "<h1> RETURN FROM THE SERVER </h1> <br>";
     $question=json_decode($return_val, true)[0];
     var_dump($question);
     $qtext = $question["Question"];
 
-    echo "<h1> CURRENT ANSWERS </h1><br>";
-    echo var_dump($answers);
+    //echo "<h1> CURRENT ANSWERS </h1><br>";
+    //echo var_dump($answers);
 
     echo "<h1> QUESTION ".$number." </h1>";
     echo "<p>" . $qtext . "</p><br>";
@@ -70,7 +70,7 @@ foreach($questions as $q){
     echo '<input type=hidden name=questions['.$i.'] value="'.$questions[$i].'">';
     echo '<input type=hidden name=answers['.$i.'] value="'.$answers[$i].'">';
     $i=$i+1;
-} 
+}
     echo '<input type=hidden name=exid value="'.$exid.'">';
     echo '<button type=submit name=submit value=submit> Submit Answers </button>';
     echo '</form>';
