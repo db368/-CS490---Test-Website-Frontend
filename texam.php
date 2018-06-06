ce1f2196673cf50b833d383d35d667b66492ce18
db368 <db368@njit.edu>
2018-06-06 09:27:40 -0400

    texam.php now correctly reads incoming question ids

--- texam.php
+++ texam.php
@@ -5,21 +5,29 @@
 <body>
 <?php
     $questions = $_POST['questions'];
-    $number = $_POST['currentnumber'];
-
+    $number = $_POST['currentquestion'];
     $qid = $questions[$number];
+   
+    echo "<h1> Incoming questions </h1>"; 
+    echo var_dump($questions);
+    echo "<br> Current Question Number is ". $number. " and the id is ". $qid . "<br>";
+
+   //CURL to get the actual useful question information; 
+    $target = "https://web.njit.edu/~jll25/CS490/switch.php";
     $ch= curl_init();
     curl_setopt($ch, CURLOPT_URL, $target);
     curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'qb_get_question', 'questionid' => $_POST["id"])));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'qb_get_question', 'questionid' => $qid)));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $return_val=curl_exec($ch);
     curl_close($ch);
-
-    $question=json_decode($return_val);
+    
+    echo "<h1> RETURN FROM THE SERVER </h1> <br>";	
+    $question=json_decode($return_val, true)[0];
+    var_dump($question);
     $qtext = $question["Question"];
 
-    echo "<h1> QUESTION ".$_POST['currentquestion']." </h1><br>";
+    echo "<h1> QUESTION ".$number." </h1><br>";
     echo "<p>" . $qtext . "</p><br>";
 
     echo '<form method="post" action="texam.php">';
@@ -32,7 +40,7 @@ foreach($questions as $q){
 if ($number < count($questions)) {
     echo '<button type=submit name=currentnumber value='.($i+1).'> Next Question </button>';
 }
-if ($number != 0) {
+if ($number != 1) {
     echo '<button type=submit name=current number value='.($i-1).'> Previous Question </button>';
 }
     echo "</form>";
