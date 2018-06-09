e7cbf1b034bb77f0cd28768dc954e714db345b70
Drew Balletto <db368@njit.edu>
2018-06-09 15:05:17 -0400

    Remove debug code

--- frontend/exams/eexam.php
+++ frontend/exams/eexam.php
@@ -56,28 +56,6 @@ th{
     if ($questions == null) {
         echo "No questions yet!";
     }
-    //No more test code
-    /*
-    for($i=0; $i<$tas; $i++){
-        array_push($examquestions, 'Question '. rand(1000, 9999));
-        array_push($examids, $i);
-        $r = rand(0, 2);
-        switch ($r){
-        case 0:
-            array_push($diffs, "Easy");
-            break;
-        case 1:
-            array_push($diffs, "Medium");
-            break;
-        case 2:
-            array_push($diffs, "Hard");
-            break;
-        default:
-                array_push($diffs, "UV"); //Dig the prowess!
-        }
-        array_push($scores, rand(1, 20));
-    }
-    */
     else{
         echo '<table style="width:100%">';
         echo '<tr><th> Question </th> <th> Difficulty </th> <th> Score </th> <th> Update </th><th> Remove </th> </tr>';
@@ -117,26 +95,6 @@ th{
         curl_close($ch);
         $questions = json_decode($return_val, true);
 
-        /* Test Data
-        for ($i = 0; $i<$tas; $i++)
-        {
-            array_push($examquestions, 'Question '. rand(1000, 9999));
-            array_push($qbids, $i);
-            //if (rand(0,20) > 15){
-            if (false) {
-                array_push($examids, $i);
-            }
-            array_push($examids, $i);
-            array_push($questionbank, "Question ". $i);
-            $numcases = rand(1, 5);
-            $cases = array();
-            for ($k=0; $k<$numcases; $k++){
-                array_push($cases, 39);
-            }
-            array_push($testcases, $cases);
-            array_push($diffs, "Difficulty");
-        }
-        */
             echo '<table style="width:100%">';
             // FOR RELEASE: echo '<tr><th> Question </th> <th> Difficulty </th> <th> Testcases </th> <th> Add to Exam </th> </tr>';
             echo '<tr><th> Question </th> <th> Difficulty </th> <th> Add to Exam </th> </tr>';
