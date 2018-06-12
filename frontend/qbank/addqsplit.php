58cf83099ed36a5c84783f0e2e721a0f3ceea3ee
db368 <db368@njit.edu>
2018-06-12 14:25:00 -0400

    Swapped out question input for a text area. Applied basic CSS styling

--- frontend/qbank/addqsplit.php
+++ frontend/qbank/addqsplit.php
@@ -106,8 +106,8 @@ div.editquestions {
         echo '> ' . $rdiff;
     }
         echo "<br>";
-        echo 'Question Text: ';
-        echo '<input type="text" name="question" value="'. $qtext . '"><br> <br>';
+        echo '<label for="question"> Question Text:</question> <br>';
+        echo '<textarea name="question" value="'. $qtext . '"> </textarea><br> <br>';
 
     for ($i = 0; $i<sizeof($testcases) || $i<$numtc; $i++){
         echo 'Test Case '. ($i + 1) .' : <input type="text" name="testcase[' . $i .']" value="'. $testcases[$i] . '">';
