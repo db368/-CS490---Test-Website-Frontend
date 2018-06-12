9971d08218fa70364074b8b953492d3eb66b2fd1
db368 <db368@njit.edu>
2018-06-12 13:39:27 -0400

    Login now passes username to landing page

--- frontend/frontend.php
+++ frontend/frontend.php
@@ -8,24 +8,38 @@ $unknownurl = "ui.html";
 
 //PHASE 1: AUTHENTICATION
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://web.njit.edu/~db368/CS490/controller.php");
+curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~db368/CS490/controller.php");
 curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('username' => $_POST["username"], 'password' => $_POST["password"], 'phase' => '1')));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $return_val=curl_exec($ch);
 curl_close($ch);
 
-$jason = json_decode($return_val);
-$role = $jason[0]->Role;
+$jason = json_decode($return_val, true)[0];
+$role = $jason['Role'];
 //PHASE 2: REDIRECTION
-if ($role == "Student") {
-    header('Location: http://web.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/frontend/student.html');
-} else if ($role == "Instructor") {
-    header('Location: http://web.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/frontend/instructor.html');
-}else{
-    header('Location: http://web.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/frontend/ui.html');
-}
-exit;
 
+//This is going to be a curl instead of headers
+switch ($role){
+	case "Student":
+	case "student":
+		 $target="https://web.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/frontend/student.php";
+        break;
+case "Instructor":
+    $target = 'https://web.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/frontend/instructor.html';
+	break;
+default:
+    $target='https://web.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/frontend/ui.html';
+	break;
+}
+//Now we curl the page based on the user's role
+$ch= curl_init();
+curl_setopt($ch, CURLOPT_URL, "$target");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$reload=curl_exec($ch);
+curl_close($ch);
+echo $reload;
 //echo $return_val;
 ?>
