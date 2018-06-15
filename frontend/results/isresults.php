bd3f137e539940d93fdb3f52a7c8de6bbf4f8028
Drew Balletto <db368@njit.edu>
2018-06-15 15:02:25 -0400

    Move more html out of php, readd exid

--- frontend/results/isresults.php
+++ frontend/results/isresults.php
@@ -29,17 +29,21 @@
 </head>
 <body>
 
-    <header> <h1> Header for <?php echo $_POST['exname'];  ?> </h1></header>
+    <header> <h1> Results for <?php echo $_POST['exname'];  ?> </h1></header>
+    <div class=login>
+
     <?php
     $debug = 1;
-    echo "<div class=login>";
-        $target = "https://web.njit.edu/~jll25/CS490/switch.php";
-        $ch= curl_init();
-        curl_setopt($ch, CURLOPT_URL, "$target");
-        curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'results', 'eid'=> $_POST['eid'])));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $return_val=curl_exec($ch);
+    $eid = ((isset($_POST['eid']))) ? $_POST['eid'] : 39;
+
+    $target = "https://web.njit.edu/~jll25/CS490/switch.php";
+    $ch= curl_init();
+    curl_setopt($ch, CURLOPT_URL, "$target");
+    curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'results', 'eid'=> $eid)));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $return_val=curl_exec($ch);
+
     $results = json_decode($return_val, true);
 
     if ($debug) {
@@ -63,14 +67,17 @@
         echo "</div>";
         echo '<br>';
     }
+
     //Check to see if we're actually getting variables here.
     if ($return_val == null) {
         echo "<h2> ERROR: EXAM LIST COULD NOT BE RETRIEVED </h2>";
         exit;
     }
     ?>
+
     <table>
     <tr> <th> STUDENT </th> <th> AVERAGE </th> </tr>
+
     <?php
     foreach($results as $student){
         echo "<tr>";
@@ -79,12 +86,12 @@
         echo '<form method="post" action="../debug.php">';
         //echo '<form method="post" action="isdresults.php">'; //Details page
         echo '<input type="hidden" name="sid" value="'.$sid.'">';
+        echo '<input type="hidden" name="exid" value="'.$exid.'">';
         echo '<td> <button type="submit" class="link-button" name="sid" value="'.$sid.'"> '.$sid.' </button> </td>';
         echo '<td>'. $score . '</td>';
         echo "</form></tr>";
     }
-    echo "</table>";
     ?>
-
+</table>
 </body>
 </html>
