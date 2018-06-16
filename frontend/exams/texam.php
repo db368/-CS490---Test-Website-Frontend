e4548a8c6ad3ee9448aa961581aba7777670924c
Drew Balletto <db368@njit.edu>
2018-06-16 13:44:11 -0400

    Reformat debug boxes, adjust indentation

--- frontend/exams/texam.php
+++ frontend/exams/texam.php
@@ -5,79 +5,80 @@
 </head>
 <body>
 
-<?php
+<?php //Initial Variable Definition
     $debug = 1;
-    $questions = (isset($_POST['questions'])) ? $_POST['questions'] : die("No questions") ;
-    $number = (isset($_POST['currentquestion'])) ? $_POST['currentquestion'] : die("No current quesiton number") ;
-    $exid = (isset($_POST['exid'])) ? $_POST['exid'] : die("No Exid") ;
+    $questions = (isset($_POST['questions'])) ? $_POST['questions'] : die("No questions");
+    $number = (isset($_POST['currentquestion'])) ? $_POST['currentquestion'] : die("No current quesiton number");
+    $exid = (isset($_POST['exid'])) ? $_POST['exid'] : die("No Exid");
     $answers = (isset($_POST['answers'])) ? $_POST['answers'] : array(); // If this isn't set, it's likely the firs time this is called so create an answers array
     $qid = $questions[$number];
+    ?>
 
-    echo "<header><h1> EXAM </h1> </header>";
-if($debug) {
-    echo "<div class=debug>";
-    echo "<h1> Incoming POST </h1>";
-    echo var_dump($_POST);
+    <header><h1> EXAM </h1> </header>
+    <?php if($debug) : ?>
+        <div class=debug>
+            <h1> Incoming POST </h1>
+            <?php echo var_dump($_POST); ?>
 
-    echo "<h1> Incoming questions </h1>";
-    echo var_dump($questions);
-    echo "<br> Current Question Number is ". $number. " anid the id is ". $qid . "<br>";
-    echo "</div>";
-}
+            <h1> Incoming questions </h1>
+            <?php echo var_dump($questions) ?> <br>
+            Current Question Number is <?php echo $number ?> " and the id is "<?php echo $qid ?> <br>
+        </div>
+    <?php
+    endif;
+          //CURL to get the actual useful question information;
+        $target = "https://web.njit.edu/~jll25/CS490/switch.php";
+        $ch= curl_init();
+        curl_setopt($ch, CURLOPT_URL, $target);
+        curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'qb_get_question', 'questionid' => $qid)));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $return_val=curl_exec($ch);
+        curl_close($ch);
 
-      //CURL to get the actual useful question information;
-    $target = "https://web.njit.edu/~jll25/CS490/switch.php";
-    $ch= curl_init();
-    curl_setopt($ch, CURLOPT_URL, $target);
-    curl_setopt($ch, CURLOPT_POST, 1); // Set it to post
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('identifier'=>'qb_get_question', 'questionid' => $qid)));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $return_val=curl_exec($ch);
-    curl_close($ch);
+        $question=json_decode($return_val, true)[0];
+        $qtext = $question["Question"];
 
-    $question=json_decode($return_val, true)[0];
-    $qtext = $question["Question"];
+        //Start actually printing the file
+        echo "<div class=login>";
+        echo "<h1> QUESTION ".$number.": </h1>";
+        echo "<p>" . $qtext . "</p><br>";
 
-    //Start actually printing the file
-    echo "<div class=login>";
-    echo "<h1> QUESTION ".$number.": </h1>";
-    echo "<p>" . $qtext . "</p><br>";
-
-    //Now we get to do the fun work of posting it again
-    echo '<form method="post" action="texam.php">';
-    $i=0;
+        //Now we get to do the fun work of posting it again
+        echo '<form method="post" action="texam.php">';
+        $i=0;
 foreach($questions as $q){
     echo '<input type=hidden name=questions['.$i.'] value="'.$questions[$i].'">';
     echo '<input type=hidden name=answer['.$i.'] value="'.$answer[$i].'">';
     $i=$i+1;
 }
 
-        echo '<input type=textbox name=answer['.$number.'] value='. $answer[$number] .'><br>';
-        echo '<input type=hidden name=exid value="'.$exid.'">';
-        echo '<input type=hidden name=sid value="'.$_POST['sid'].'">';
+            echo '<input type=textbox name=answer['.$number.'] value='. $answer[$number] .'><br>';
+            echo '<input type=hidden name=exid value="'.$exid.'">';
+            echo '<input type=hidden name=sid value="'.$_POST['sid'].'">';
 if ($number < count($questions)-1) {
     echo '<button type=submit name=currentquestion value='.($number+1).'> Next Question </button>';
 }
 if ($number != 1) {
     echo '<button type=submit name=currentquestion value='.($number-1).'> Previous Question </button>';
 }
-    echo '<button type=submit name=identifier value=answer formaction="examlandingpage.php"> Submit answer </button>';
-        echo "</form>";
-    // Submit button stuff
-     echo '<form method="post" action="examlandingpage.php">';
-    //echo '<form method="post" action="../debug.php">';
-    $i=0;
+        echo '<button type=submit name=identifier value=answer formaction="examlandingpage.php"> Submit answer </button>';
+            echo "</form>";
+        // Submit button stuff
+         echo '<form method="post" action="examlandingpage.php">';
+        //echo '<form method="post" action="../debug.php">';
+        $i=0;
 foreach($questions as $q){
     echo '<input type=hidden name=questions['.$i.'] value="'.$questions[$i].'">';
     echo '<input type=hidden name=answer['.$i.'] value="'.$answer[$i].'">';
     $i=$i+1;
 }
-    echo '<input type=hidden name=exid value="'.$exid.'">';
-    echo '<input type=hidden name=sid value="'.$_POST['sid'].'">';
-    echo '<button type=submit name=identifier value=answer> Submit answer </button>';
-    echo '</form>';
-    echo "</div>";
-?>
+        echo '<input type=hidden name=exid value="'.$exid.'">';
+        echo '<input type=hidden name=sid value="'.$_POST['sid'].'">';
+        echo '<button type=submit name=identifier value=answer> Submit answer </button>';
+        echo '</form>';
+        echo "</div>";
+    ?>
 
 
 </body>
