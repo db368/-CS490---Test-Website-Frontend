f6292ce318833714c401c87da8805c3dce2b1610
Drew Balletto <db368@njit.edu>
2018-06-19 20:22:25 -0400

    Connect to debug looper

--- frontend/results/sdresults.php
+++ frontend/results/sdresults.php
@@ -100,7 +100,9 @@ bad{
         for($i=0; $i < sizeof($results); $i++){
             $res_qid = $results[$i]['Qid'];
             $target = "https://web.njit.edu/~jll25/CS490/switch.php";
+            $target = "https://web.njit.edu/~db368/CS490_git/CS490-Test-Website-Frontend/frontend/loopers/debuglooper.php";
             $postarray = array('identifier'=>'g_comment','qid'=> $res_qid,'sid' => $sid,'exid' => $eid);
+
             var_dump($postarray);
             $ch= curl_init();
             curl_setopt($ch, CURLOPT_URL, "$target");
@@ -110,9 +112,11 @@ bad{
                     $postarray);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $return_val2=curl_exec($ch);
-            echo $return_val2;
-            curl_close($ch);
 
+
+            echo $return_val2;
+
+            curl_close($ch);
             $comment = json_decode($return_val2, true);
             $results[$i]['comment'] = $comment['Comments'];
             $results[$i]['newgrade'] = $comment['Score'];
