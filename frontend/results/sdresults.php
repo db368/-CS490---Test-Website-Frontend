d9f7c13c3e5ef054f9f985519a17d9e38584bd8b
Drew Balletto <db368@njit.edu>
2018-06-19 20:31:03 -0400

    Forgot a semi

--- frontend/results/sdresults.php
+++ frontend/results/sdresults.php
@@ -113,7 +113,7 @@ bad{
                     $postarray);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $return_val2= curl_exec($ch) ;
-            echo $return_val2
+            echo $return_val2;
 
             curl_close($ch);
             $comment = json_decode($return_val2, true);
